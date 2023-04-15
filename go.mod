fe9427b0003715343f3880674f18696c71963cab
Milofow <milofow@hotmail.nl>
2023-04-15 14:02:18 +0200

    GRPC test code

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/Portfolio-Advanced-software/BingeBuster-UserService
 go 1.20
 
 require github.com/rabbitmq/amqp091-go v1.8.0
+
+require google.golang.org/protobuf v1.30.0 // indirect
---
+++ models/User.go
@@ -0,0 +1,10 @@
+package models
+
+
+type User struct {
+	ID       string   `bson:"_id,omitempty"`
+	Name     string   `bson:"name,omitempty"`
+	Email    string   `bson:"email,omitempty"`
+	Password string   `bson:"password,omitempty"`
+	Movies   []string `bson:"movies,omitempty"`
+}
