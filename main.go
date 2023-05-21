bc6463f301685f34a5cca80d99141803e457beb9
Milofow <Milofow@hotmail.nl>
2023-05-21 13:28:48 +0200

    pushed for proto gen on mac

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"os/signal"
 
 	models "github.com/Portfolio-Advanced-software/BingeBuster-UserService/models"
-	mongodb "github.com/Portfolio-Advanced-software/BingeBuster-UserService/mongo"
+	mongodb "github.com/Portfolio-Advanced-software/BingeBuster-UserService/mongodb"
 	userpb "github.com/Portfolio-Advanced-software/BingeBuster-UserService/proto"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -25,15 +25,22 @@ type UserServiceServer struct {
 }
 
 func (s *UserServiceServer) CreateUser(ctx context.Context, req *userpb.CreateUserReq) (*userpb.CreateUserRes, error) {
-	// Essentially doing req.User to access the struct with a nil check
+	// Essentially doing req.Movie to access the struct with a nil check
 	user := req.GetUser()
 	if user == nil {
 		return nil, status.Error(codes.InvalidArgument, "Invalid user")
 	}
-	// Now we have to convert this into a User type to convert into BSON
+	// Now we have to convert this into a Movie type to convert into BSON
 	data := models.User{
 		// ID:    Empty, so it gets omitted and MongoDB generates a unique Object ID upon insertion.
-		WatchedMovies: user.GetWatchedmovies(),
+		Email: user.GetEmail(),            
+		Phone: user.GetPhone();                
+		DateOfBirth: user.GetDateOfBirth();             
+		FirstName: user.;             
+		LastName: user.;              
+		CreditCardNumber: user.;           
+		ExpirationDate: user.;       
+		CVC: user.; 
 	}
 
 	// Insert the data into the database, result contains the newly generated Object ID for the new document
@@ -46,11 +53,11 @@ func (s *UserServiceServer) CreateUser(ctx context.Context, req *userpb.CreateUs
 			fmt.Sprintf("Internal error: %v", err),
 		)
 	}
-	// add the id to user, first cast the "generic type" (go doesn't have real generics yet) to an Object ID.
+	// add the id to movie, first cast the "generic type" (go doesn't have real generics yet) to an Object ID.
 	oid := result.InsertedID.(primitive.ObjectID)
 	// Convert the object id to it's string counterpart
 	user.Id = oid.Hex()
-	// return the blog in a CreateUserRes type
+	// return the blog in a CreateMovieRes type
 	return &userpb.CreateUserRes{User: user}, nil
 }
 
@@ -61,27 +68,34 @@ func (s *UserServiceServer) ReadUser(ctx context.Context, req *userpb.ReadUserRe
 		return nil, status.Errorf(codes.InvalidArgument, fmt.Sprintf("Could not convert to ObjectId: %v", err))
 	}
 	result := userdb.FindOne(ctx, bson.M{"_id": oid})
-	// Create an empty User to write our decode result to
+	// Create an empty user to write our decode result to
 	data := models.User{}
 	// decode and write to data
 	if err := result.Decode(&data); err != nil {
 		return nil, status.Errorf(codes.NotFound, fmt.Sprintf("Could not find user with Object Id %s: %v", req.GetId(), err))
 	}
-	// Cast to ReadUserRes type
-	response := &userpb.ReadUserRes{
-		User: &userpb.User{
-			Id:            oid.Hex(),
-			Watchedmovies: data.WatchedMovies,
+	// Cast to ReadMovieRes type
+	response := &userpb.ReadMovieRes{
+		Movie: &userpb.Movie{
+			Id:          oid.Hex(),
+			Title:       data.Title,
+			Description: data.Description,
+			ReleaseDate: data.ReleaseDate,
+			Director:    data.Director,
+			Genre:       data.Genre,
+			Rating:      data.Rating,
+			Runtime:     data.Runtime,
+			Poster:      data.Poster,
 		},
 	}
 	return response, nil
 }
 
-func (s *UserServiceServer) ListUsers(req *userpb.ListUserReq, stream userpb.UserService_ListUsersServer) error {
-	// Initiate a User type to write decoded data to
+func (s *UserServiceServer) ListUsers(req *userpb.ListMoviesReq, stream userpb.UserService_ListUsersServer) error {
+	// Initiate a movie type to write decoded data to
 	data := &models.User{}
 	// collection.Find returns a cursor for our (empty) query
-	cursor, err := userdb.Find(context.Background(), bson.M{})
+	cursor, err := moviedb.Find(context.Background(), bson.M{})
 	if err != nil {
 		return status.Errorf(codes.Internal, fmt.Sprintf("Unknown internal error: %v", err))
 	}
@@ -96,10 +110,17 @@ func (s *UserServiceServer) ListUsers(req *userpb.ListUserReq, stream userpb.Use
 			return status.Errorf(codes.Unavailable, fmt.Sprintf("Could not decode data: %v", err))
 		}
 		// If no error is found send blog over stream
-		stream.Send(&userpb.ListUserRes{
-			User: &userpb.User{
-				Id:            data.ID.Hex(),
-				Watchedmovies: data.WatchedMovies,
+		stream.Send(&userpb.ListUsersRes{
+			Movie: &userpb.User{
+				Id:          data.ID.Hex(),
+				Title:       data.Title,
+				Description: data.Description,
+				ReleaseDate: data.ReleaseDate,
+				Director:    data.Director,
+				Genre:       data.Genre,
+				Rating:      data.Rating,
+				Runtime:     data.Runtime,
+				Poster:      data.Poster,
 			},
 		})
 	}
@@ -112,7 +133,7 @@ func (s *UserServiceServer) ListUsers(req *userpb.ListUserReq, stream userpb.Use
 
 func (s *UserServiceServer) UpdateUser(ctx context.Context, req *userpb.UpdateUserReq) (*userpb.UpdateUserRes, error) {
 	// Get the user data from the request
-	user := req.GetUser()
+	movie := req.GetMovie()
 
 	// Convert the Id string to a MongoDB ObjectId
 	oid, err := primitive.ObjectIDFromHex(user.GetId())
@@ -125,7 +146,14 @@ func (s *UserServiceServer) UpdateUser(ctx context.Context, req *userpb.UpdateUs
 
 	// Convert the data to be updated into an unordered Bson document
 	update := bson.M{
-		"watchedmovies": user.GetWatchedmovies(),
+		"title":       movie.GetTitle(),
+		"description": movie.GetDescription(),
+		"releaseDate": movie.GetReleaseDate(),
+		"director":    movie.GetDirector(),
+		"genre":       movie.GetGenre(),
+		"rating":      movie.GetRating(),
+		"runtime":     movie.GetRuntime(),
+		"poster":      movie.GetPoster(),
 	}
 
 	// Convert the oid into an unordered bson document to search by id
@@ -133,7 +161,7 @@ func (s *UserServiceServer) UpdateUser(ctx context.Context, req *userpb.UpdateUs
 
 	// Result is the BSON encoded result
 	// To return the updated document instead of original we have to add options.
-	result := userdb.FindOneAndUpdate(ctx, filter, bson.M{"$set": update}, options.FindOneAndUpdate().SetReturnDocument(1))
+	result := moviedb.FindOneAndUpdate(ctx, filter, bson.M{"$set": update}, options.FindOneAndUpdate().SetReturnDocument(1))
 
 	// Decode result and write it to 'decoded'
 	decoded := models.User{}
@@ -145,9 +173,16 @@ func (s *UserServiceServer) UpdateUser(ctx context.Context, req *userpb.UpdateUs
 		)
 	}
 	return &userpb.UpdateUserRes{
-		User: &userpb.User{
-			Id:            decoded.ID.Hex(),
-			Watchedmovies: decoded.WatchedMovies,
+		Movie: &userpb.Movie{
+			Id:          decoded.ID.Hex(),
+			Title:       decoded.Title,
+			Description: decoded.Description,
+			ReleaseDate: decoded.ReleaseDate,
+			Director:    decoded.Director,
+			Genre:       decoded.Genre,
+			Rating:      decoded.Rating,
+			Runtime:     decoded.Runtime,
+			Poster:      decoded.Poster,
 		},
 	}, nil
 }
@@ -167,28 +202,31 @@ func (s *UserServiceServer) DeleteUser(ctx context.Context, req *userpb.DeleteUs
 		return nil, status.Errorf(codes.NotFound, fmt.Sprintf("Could not find/delete user with id %s: %v", req.GetId(), err))
 	}
 	// Return response with success: true if no error is thrown (and thus document is removed)
-	return &userpb.DeleteUserRes{
+	return &userpb.DeleteMovieRes{
 		Success: true,
 	}, nil
 }
 
 const (
-	port = ":50051"
+	port = ":50055"
 )
 
 var db *mongo.Client
-var userdb *mongo.Collection
+var moviedb *mongo.Collection
 var mongoCtx context.Context
 
 var mongoUsername = "user-service"
 var mongoPwd = "vLxxhmS0eJFwmteF"
-var connUri = "mongodb+srv://" + mongoUsername + ":" + mongoPwd + "@cluster0.fpedw5d.mongodb.net/test"
+var connUri = "mongodb+srv://" + mongoUsername + ":" + mongoPwd + "@cluster0.fpedw5d.mongodb.net/"
+
+var dbName = "UserService"
+var collectionName = "Users"
 
 func main() {
 	// Configure 'log' package to give file name and line number on eg. log.Fatal
 	// Pipe flags to one another (log.LstdFLags = log.Ldate | log.Ltime)
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	fmt.Println("Starting server on port :50051...")
+	fmt.Println("Starting server on port :50055...")
 
 	// Set listener to start server
 	lis, err := net.Listen("tcp", port)
@@ -204,21 +242,21 @@ func main() {
 	srv := &UserServiceServer{}
 
 	// Register the service with the server
-	userpb.RegisterUserServiceServer(s, srv)
+	userpb.RegisterMovieServiceServer(s, srv)
 
 	// Initialize MongoDb client
 	fmt.Println("Connecting to MongoDB...")
 	db = mongodb.ConnectToMongoDB(connUri)
 
 	// Bind our collection to our global variable for use in other methods
-	userdb = db.Database("MovieHistoryService").Collection("Watched")
+	moviedb = db.Database(dbName).Collection(collectionName)
 
 	go func() {
 		if err := s.Serve(lis); err != nil {
 			log.Fatalf("Failed to serve: %v", err)
 		}
 	}()
-	fmt.Println("Server succesfully started on port :50051")
+	fmt.Println("Server succesfully started on port :50055")
 
 	// Right way to stop the server using a SHUTDOWN HOOK
 	// Create a channel to receive OS signals
--- models/User.go
+++ models/User.go
@@ -3,6 +3,13 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID            primitive.ObjectID `bson:"_id,omitempty"`
-	WatchedMovies string             `bson:"watchedmovies,omitempty"`
+	ID               primitive.ObjectID `bson:"_id,omitempty"`
+	Email            string             `bson:"email,omitempty"`
+	Phone            string             `bson:"phone,omitempty"`
+	DateOfBirth      string             `bson:"dateofbirth,omitempty"`
+	FirstName        string             `bson:"firstname,omitempty"`
+	LastName         string             `bson:"lastname,omitempty"`
+	CreditCardNumber int32              `bson:"creditcardnumber,omitempty"`
+	ExpirationDate   string             `bson:"expirationdate,omitempty"`
+	CVC              int32              `bson:"cvc,omitempty"`
 }
